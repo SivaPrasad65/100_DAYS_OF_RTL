4004035488f7a81e9993d0e5c9ed363d0728ee8c
SIVA PRASAD <155300848+SivaPrasad65@users.noreply.github.com>
2024-02-21 20:45:03 +0530

    DAY-052

---
+++ DAY-052/Moore_nonoverlap_tb.v
@@ -0,0 +1,36 @@
+`timescale 1ns / 1ps
+
+
+module Moore_nonoverlap_tb;
+reg din,clk,reset;
+ wire  dout;
+ Moore_Nonoverlapping dut(din,clk,reset,dout);
+always #50 clk = ~clk;
+
+    initial begin
+        reset = 1;
+        din = 0;
+        clk = 0;
+        #100; reset = 0; 
+        din = 1;
+        #100; din = 1;
+        #100; din = 0;
+        #100; din = 0;
+        #100; din = 1;
+        #100; din = 0;
+        #100; din = 0;
+        #100; din = 1;
+        #100; din = 0;
+        #100; din = 0;
+        #100; din = 1;
+        #100; din = 0;
+        #100; din = 0;
+
+    end
+
+    initial begin
+        $monitor("clk=%b, reset=%b, din=%b, dout=%b", clk, reset, din, dout);
+        #2000 $finish;
+    end
+          
+endmodule
