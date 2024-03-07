c4a4a3b01ed0842e7725e59ddd1db86b86000205
SIVA PRASAD <155300848+SivaPrasad65@users.noreply.github.com>
2024-03-07 18:40:35 +0530

    DAY-067

---
+++ DAY-067/Booth_multiplier_tb.v
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+
+module Booth_multiplier_tb;
+reg signed [3:0] multiplicand;
+  reg signed [3:0] multiplier;
+  wire signed [7:0] product;
+  
+  BOOTHS_MULTIPLIER dut (multiplicand, multiplier, product);
+
+  initial begin
+    $monitor("multiplicand = %d, multiplier = %d, product = %d", multiplicand, multiplier, product);
+    multiplicand = 4'b0011;
+    multiplier = 4'b0001;
+    #10;
+    multiplicand = 4'b1010;
+    multiplier = 4'b0111;
+    #10;
+    multiplicand = 4'b1110;
+    multiplier = 4'b1011;
+    #10;
+    multiplicand = 4'b1011;
+    multiplier = 4'b1011;
+    #10;
+    $finish;
+  end
+endmodule
+
