a51b704d7cdae80dc14d5de4ac9001f7264d8df2
SIVA PRASAD <155300848+SivaPrasad65@users.noreply.github.com>
2024-03-15 20:02:08 +0530

    DAY-075

---
+++ DAY-075/factorial_tb.v
@@ -0,0 +1,16 @@
+`timescale 1ns / 1ps
+
+
+module factorial_tb;
+reg [3:0] n;
+wire [31:0] result;
+integer x;
+factorial dut(n, result);
+
+always for(x=0; x<11; x=x+1) #10 n=x;
+
+initial begin 
+$monitor("\t\t factorial of %d is %0d", n, result);
+#120 $finish;
+end
+endmodule
