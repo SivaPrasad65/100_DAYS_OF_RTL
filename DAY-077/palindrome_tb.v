cd9f877cf80b827a8cbc6ea755b545b3a2d0cd23
SIVA PRASAD <155300848+SivaPrasad65@users.noreply.github.com>
2024-03-17 18:59:18 +0530

    DAY-077

---
+++ DAY-077/palindrome_tb.v
@@ -0,0 +1,31 @@
+`timescale 1ns / 1ps
+
+
+module palindrome_tb;
+reg [15:0]number;
+wire is_palindrome;
+
+Palindrome dut(number, is_palindrome);
+
+always 
+begin
+number=16'd18681;
+#10;
+number=16'd3453;
+#10;
+number=16'd14441;
+#10;
+number=16'd51125;
+#10;
+number=16'd12021;
+#10;
+number=16'd35153;
+#10;
+number=16'd8998;
+#10;
+number=16'd6196;
+#10;
+end
+
+initial #80 $finish;
+endmodule
